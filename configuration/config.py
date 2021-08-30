774bcb0929decd4d034cdcc25fe6c802f4866c75
@iamsannyrai <iam.sannyrai@gmail.com>
2021-08-30 09:38:54 +0545

    setup posgres as db for development

--- configuration/config.py
+++ configuration/config.py
@@ -1,12 +1,18 @@
+import os
+from dotenv import load_dotenv
 from core.enums import Environment
+
+load_dotenv()
 
 
 class Config:
     DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(Config):
     ENV = "development"
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     pass
 
 
--- init.py
+++ init.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 from core.enums import Environment
 from configuration.config import config
+from core.extensions import db
 
 
 def create_app(environment: Environment):
     app = Flask(__name__)
     app.config.from_object(config[environment])
+    db.init_app(app)
     return app
