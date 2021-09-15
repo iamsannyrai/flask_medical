41c864d43c4a6c4fef61bb55406fe332cda39c38
@iamsannyrai <iam.sannyrai@gmail.com>
2021-09-15 21:44:51 +0545

    change to staticmethod

--- applications/admin_app/resources/role_resource.py
+++ applications/admin_app/resources/role_resource.py
@@ -8,23 +8,23 @@ from applications.admin_app.schemas.role_schema import RoleSchema
 class RoleResource(Resource):
 
     def __init__(self):
-        self.admin_app_manager = AdminAppManager()
         self.role_schema = RoleSchema()
         self.role_schemas = RoleSchema(many=True)
 
     def get(self, role_id=None):
         if role_id:
-            role = self.admin_app_manager.get_role(role_id)
+            role = AdminAppManager.get_role(role_id)
             return self.role_schema.dump(role)
         else:
-            roles = self.admin_app_manager.get_roles()
+            roles = AdminAppManager.get_roles()
             return self.role_schemas.dump(roles)
 
     def post(self):
         role_dict = request.get_json()
-        role = self.admin_app_manager.create_role(role_dict['role'])
+        role = AdminAppManager.create_role(role_dict['role'])
         return self.role_schema.dump(role), 201
 
-    def delete(self, role_id):
-        self.admin_app_manager.delete_role(role_id)
+    @staticmethod
+    def delete(role_id):
+        AdminAppManager.delete_role(role_id)
         return {}, 204
